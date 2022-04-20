8ff159e2f47bc2b7b4a3678aca72b4ee6168b10a
KonradWGitHub <konwiech@gmail.com>
2022-04-20 13:18:07 +0200

    Dodano możliwość edycji drabinki i poprawiono widok mobilny strony

--- src/components/PopUp.jsx
+++ src/components/PopUp.jsx
@@ -1,33 +1,80 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
+// import { TournamentDataContext } from "../tournamentDataContext";
 
 const PopUp = (props) => {
-  const { date, FPscore, FPname, SPscore, SPname, id, togglePopUp } = props;
+  const {
+    updateTournamentData,
+    isUpper,
+    round,
+    date,
+    FPscore,
+    FPname,
+    SPscore,
+    SPname,
+    id,
+    togglePopUp,
+  } = props;
+
+  // const context = useContext(TournamentDataContext);
+  // console.log(context);
+
+  const [formData, setFormData] = useState({
+    FPscore: FPscore,
+    SPscore: SPscore,
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    updateTournamentData(
+      isUpper,
+      round,
+      id,
+      formData.FPscore,
+      formData.SPscore
+    );
+  };
+
+  const handleChange = (e) => {
+    setFormData((prevData) => ({ ...prevData, [e.target.id]: e.target.value }));
+  };
+
   return (
     <>
       <div className="popup-container">
-        <div className="popup-box">
-          <span className="popup-close" onClick={togglePopUp}>
+        <form className="popup-box">
+          <div className="popup-close" onClick={togglePopUp}>
             &times;{" "}
-          </span>
-          <p onClick={console.log(props)}>Clicked game with ID: {id}</p>
+          </div>
+          {/* <p onClick={console.log(props)}>Clicked game with ID: {id}</p> */}
           <div className="popup-game">
             <div className="popup-contestant">
               <span className="popup-contestant-name">{FPname}</span>
               <input
                 className="popup-contestant-score"
-                placeholder={FPscore}
+                id="FPscore"
+                value={formData.FPscore}
+                onChange={handleChange}
               ></input>
             </div>
             <div className="popup-contestant">
               <span className="popup-contestant-name">{SPname}</span>
               <input
                 className="popup-contestant-score"
-                placeholder={SPscore}
+                id="SPscore"
+                value={formData.SPscore}
+                onChange={handleChange}
               ></input>
             </div>
           </div>
-          <button>Submit</button>
-        </div>
+          <button
+            type="submit"
+            onClick={handleSubmit}
+            className="popup-submit-btn"
+          >
+            Submit
+          </button>
+        </form>
       </div>
     </>
   );
